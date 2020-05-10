8847ebdba37076cccf0b2a31b77fbfc254185e7e
wudeyuan <wudeyuan2016@outlook.com>
2020-05-10 15:17:40 +0800

    Update app_auto.py

--- app_auto.py
+++ app_auto.py
@@ -1,5 +1,5 @@
-# 需要多订阅（一般大于10）更新比较快的刊物，使得每天有6篇以上的“当日文章”，文章选取是在订阅模块
-# 订阅不会更新很早，建议在12点半以后刷。订阅里面不要有“只有几张图片的那种文章”（有极个别订阅号会有这种文章），这种文章上滑就会返回。
+# 需要多订阅（一般大于10）更新比较快的刊物，使得每天有6篇以上的新文章，两次刷分时间距离不要太短。文章选取是在订阅模块
+# 订阅里面不要有“只有几张图片的那种文章”（有极个别订阅号会有这种文章），这种文章上滑就会返回。
 # d.click()里面位置虽然是百分比，但是不同的手机位置可能还是有区别的，需要自己调试“返回”等在哪
 # “.......................”表示该行需要根据具体手机做调整
 # swipe的x，y轴的值可能需要修改（手机屏幕小于5寸），也可能不需要修改（屏幕较大）
@@ -17,8 +17,7 @@ class auto_app():
     # 今天，昨天
     def __init__(self):
         today = datetime.datetime.today().date()
-        self.date_now = str(today)
-        self.date_yesterday = str(today-datetime.timedelta(days=1))
+        self.date_mon = str(today)[:8] # 如2020-05-
         self.x1=800 # swipe的x轴值
     # 点击返回
     def back(self):
@@ -65,15 +64,17 @@ class auto_app():
         d.xpath("//*[@text='订阅']").click()
         time.sleep(rand(1))
         d.xpath("//*[@text='订阅']").click()
+        time.sleep(rand(1))
+        d.swipe(rand(self.x1),rand(700),rand(self.x1),rand(1000)) # 刷新一下，一般不需要，但有时候会出问题 
         time.sleep(rand(2))
-        articlelist1=d.xpath("//*[@text='%s']" % self.date_now).all() # 跟selenium语法有所不同
+        articlelist1=d.xpath("//*[contains(@text,'%s')]" % self.date_mon).all() # 跟selenium语法有所不同
         print("阅读文章：",len(articlelist1))
         mix_article=articlelist1[0:2]
         self.mix(mix_article=mix_article)
         self.read(articlelist1,11) # 阅读
         time.sleep(rand(2))
         [d.swipe(rand(self.x1),rand(1000),rand(self.x1),rand(400)) for x in range(3)]
-        articlelist0=d.xpath("//*[@text='%s']" % self.date_now).all()
+        articlelist0=d.xpath("//*[contains(@text,'%s')]" % self.date_mon).all()
         print("阅读文章：",len(articlelist0[-(6-len(articlelist1)):]))
         self.read(set(articlelist0[-(6-len(articlelist1)):]),11)
         time.sleep(rand(2))
@@ -98,13 +99,13 @@ class auto_app():
         time.sleep(rand(2))
         d.xpath("//*[@text='联播频道']").click()
         time.sleep(rand(5))
-        videolist0=d.xpath("//*[@text='%s']" % self.date_yesterday).all()
+        videolist0=d.xpath("//*[contains(@text,'%s')]" % self.date_mon).all()
         print("观看视频：",len(videolist0))
         self.watch(videolist0,45) # 这里面有新闻联播，如果发现视频刷的时间总短了一些，可以增加第二个参数的值
         time.sleep(rand(2))
         [d.swipe(rand(self.x1),rand(1000),rand(self.x1),rand(460)) for x in range(2)] # 滑动屏幕获取更多的链接
         time.sleep(rand(1))
-        videolist=d.xpath("//*[@text='%s']" % self.date_yesterday).all() # 跟selenium语法有所不同
+        videolist=d.xpath("//*[contains(@text,'%s')]" % self.date_mon).all() # 跟selenium语法有所不同
         print("观看视频：",len(videolist[-(6-len(videolist0)):]))
         self.watch(set(videolist[-(6-len(videolist0)):]),36)
           
@@ -112,8 +113,8 @@ if __name__ == "__main__":
     A=True; B=False
     # d=u2.connect('192.168.31***:89**') # wifi连接
     # 端口可以连接usb时在cmd中设置 adb tcpip 5566 
-    # wifi连接不上时，可以先连接usb启动
-    d=u2.connect('872QEDU82***') # 手机名字.......................
+    # wifi连接不上时，可以先连接usb启动，再尝试wifi连接
+    d=u2.connect('872QEDU822KP4') # 手机名字.......................
     read=auto_app()
     beign = datetime.datetime.today()
     if B:
@@ -125,11 +126,12 @@ if __name__ == "__main__":
         d.app_start('cn.xuexi.android') # 学习强国
         time.sleep(rand(2))
         d.click(0.934, 0.137) # 右上角的那几杠.......................
-        time.sleep(2)
+        time.sleep(rand(2))
         d.xpath("//*[@text='北京']").click() # 本地频道，不同地区会所有不同.......................
         time.sleep(rand(1))
         d(text="北京学习平台").click(timeout=3) # 本地频道，不同地区会所有不同.......................
-        time.sleep(rand(2))
+        time.sleep(rand(1))
+        d(text="推荐").click(timeout=10)
         read.back()       
         read.article()
         read.video()
